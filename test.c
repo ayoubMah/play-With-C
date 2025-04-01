472320afec9e65f6683c53ad9ee0cdfac3a85659
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-04-01 23:35:19 +0200

    Make a test

---
+++ test.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+int main (void)
+{
+	int r  = 5;
+	float s = 5.6 ;
+
+
+	printf("%d and %10.3e\n" , r , s) ;
+	printf("%3d and %f\n" , r , s) ;
+	printf("%5.3d and %f\n" , r , s) ;
+	printf("%d and %f\n" , r , s) ;
+	printf("%d and %f\n" , r , s) ;
+	printf("%d and %f\n" , r , s) ;
+	return 0 ;
+
+
+}
