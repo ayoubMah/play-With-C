b480e27dbd2e14a8b5fc46edc56542119587da8d
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-04-01 13:17:04 +0200

    Fix the error

--- claculator.c
+++ claculator.c
@@ -1,15 +1,19 @@
 # include <stdio.h>
+# define PI 3.14
 
 int main(void)
 {
 	printf("============= Welcome X + Y Calculator ===============\n");
-	int x, y , result ;
+	int x, y , result;
 	printf("Please Enter the First Number : \n");
 	scanf("%d" , &x);
 	printf("Please Enter the Second Number : \n");
 	scanf("%d", &y); 
 	result = x + y ;
 	printf ("The Result of %d+%d = %d\n",x ,y ,result) ;
+
+	printf("The surface of a cercle with r = x is %f/n", x * x * PI);
+
 
 	return 0 ;
 
